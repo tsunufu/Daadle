554933f5c4a307fabb5d5817d72ed64aa892e22c
tsunufu <tsunufu@icloud.com>
2024-02-14 09:40:02 +0900

    init

---
+++ running.io/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  running.io
+//
+//  Created by ryo on 2024/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ running.io/running_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  running_ioApp.swift
+//  running.io
+//
+//  Created by ryo on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct running_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
