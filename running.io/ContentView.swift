7ab99ff5c4596d06eb470db736445fa12e704231
tsunufu <tsunufu@icloud.com>
2024-02-15 12:16:49 +0900

    #1 - bugfix: fixed Modifying state during view update, this will cause undefined behavior.

--- running.io/ContentView.swift
+++ running.io/ContentView.swift
@@ -8,35 +8,17 @@
 import SwiftUI
 import MapKit
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
 struct FullScreenMapView: View {
-    @StateObject private var locationManager = LocationManager()
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 35.6895, longitude: 139.6917),
-        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
-    )
+    @ObservedObject private var locationManager = LocationManager()
+    
+    @State  var trackingMode = MapUserTrackingMode.follow
 
     var body: some View {
-        Map(coordinateRegion: $region)
+        
+        Map(coordinateRegion: $locationManager.region,
+            showsUserLocation: true,
+            userTrackingMode: $trackingMode)
             .edgesIgnoringSafeArea(.all)
-            .environmentObject(locationManager)
     }
 }
 
--- running.io/LocationManager.swift
+++ running.io/LocationManager.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 import CoreLocation
+import MapKit
 
 class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     @Published var location: CLLocation?
-    var locationManager: CLLocationManager 
+    var locationManager: CLLocationManager
+    @Published  var region =  MKCoordinateRegion()
 
     override init() {
         locationManager = CLLocationManager()
-        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
-        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = 3.0
+        
+        locationManager.requestAlwaysAuthorization()
+        
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.pausesLocationUpdatesAutomatically = false
         
         super.init()
         locationManager.delegate = self
@@ -25,10 +32,28 @@ class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
     
     
     func locationManager(_ manager: CLLocationManager,
-                         didUpdateLocations locations: [CLLocation]){
+                         didUpdateLocations locations: [CLLocation], didChangeAuthorization status: CLAuthorizationStatus){
+        
+            if status == .authorizedAlways {
+                locationManager.allowsBackgroundLocationUpdates = true
+                locationManager.startUpdatingLocation()
+            }
+
         
         if let newLocation = locations.last{
             self.location = newLocation
+            locations.last.map {
+                let center = CLLocationCoordinate2D(
+                    latitude: $0.coordinate.latitude,
+                    longitude: $0.coordinate.longitude)
+
+                    // 地図を表示するための領域を再構築
+                    region = MKCoordinateRegion(
+                        center: center,
+                        latitudinalMeters: 1000.0,
+                        longitudinalMeters: 1000.0
+                )
+            }
             print("(\(newLocation.coordinate.latitude), \(newLocation.coordinate.longitude))")
         }
         
