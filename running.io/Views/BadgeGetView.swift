def7115048bbc80b8e47e0f77d4c9c568b9830ab
tsunufu <tsunufu@icloud.com>
2024-04-30 22:39:51 +0900

    #83 - implemented badge system

---
+++ running.io/Views/BadgeGetView.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeGetView.swift
+//  running.io
+//
+//  Created by ryo on 2024/04/29.
+//
+
+import SwiftUI
+
+struct BadgeGetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeGetView()
+}
